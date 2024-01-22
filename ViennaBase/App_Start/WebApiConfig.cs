50074937e1797853b0b0a06cc7a6707bf010d296
Pre Live <preliveserver@viennasolutions.com>
2024-01-22 06:59:55 +0000

    2.3.2.2

--- ViennaBase/App_Start/WebApiConfig.cs
+++ ViennaBase/App_Start/WebApiConfig.cs
@@ -12,12 +12,13 @@ namespace ViennaBase
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-            config.EnsureInitialized();
+           // config.EnsureInitialized();
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
             // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
@@ -42,6 +43,7 @@ namespace ViennaBase
             {
  
             }
+            config.EnsureInitialized();
 
         }
     }
