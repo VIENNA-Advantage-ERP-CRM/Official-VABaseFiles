71c491c9aae5e739fb8b8397a93c13055fae6041
Pre Live <preliveserver@viennasolutions.com>
2024-04-24 06:35:29 +0000

    add route api

--- ViennaBase/App_Start/WebApiConfig.cs
+++ ViennaBase/App_Start/WebApiConfig.cs
@@ -18,7 +18,13 @@ namespace ViennaBase
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-           // config.EnsureInitialized();
+            //old
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi_1",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            // config.EnsureInitialized();
             // Uncomment the following line of code to enable query support for actions with an IQueryable or IQueryable<T> return type.
             // To avoid processing unexpected or malicious queries, use the validation settings on QueryableAttribute to validate incoming queries.
             // For more information, visit http://go.microsoft.com/fwlink/?LinkId=279712.
