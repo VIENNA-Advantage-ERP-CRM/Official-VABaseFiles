7489ab63624c4695b6af015f11225f558de5a273
Pre Live <preliveserver@viennasolutions.com>
2024-05-13 13:51:35 +0000

    5.6. .  beta

--- BaseLibrary/Audit/MSession.cs
+++ BaseLibrary/Audit/MSession.cs
@@ -186,7 +186,7 @@ namespace VAdvantage.Model
 
         public static MSession GetByWebSessionID(string websessionId)
         {
-            var id = DB.GetSQLValue(null,"SELECT AD_Session_ID FROM AD_Session WHERE WebSession = '" + websessionId + "' AND IsProcessed='Y' ");
+            var id = DB.GetSQLValue(null,"SELECT AD_Session_ID FROM AD_Session WHERE WebSession = '" + websessionId + "' AND Processed='Y' ");
             if (id > 0)
             {
                 MSession s = new MSession(new Ctx(), id, null);
