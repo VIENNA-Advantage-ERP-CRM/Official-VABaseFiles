fdc8e8f7cb858a190bbc45a4f195c5b073a2a8c3
mandeep.s <mandeep.s@viennasolutions.com>
2024-12-17 12:24:30 +0530

    SSO Code

--- ViennaBase/App_Start/AuthConfig.cs
+++ ViennaBase/App_Start/AuthConfig.cs
@@ -1,32 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-//using Microsoft.Web.WebPages.OAuth;
+﻿using CoreLibrary.DataBase;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin;
+using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.Owin.Infrastructure;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.Data;
+using System.Globalization;
+using VAdvantage.Utility;
 
 
 namespace ViennaBase
 {
     public static class AuthConfig
     {
-        public static void RegisterAuth()
+        public static void RegisterAuth(IAppBuilder app)
         {
-            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
-            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
-
-            //OAuthWebSecurity.RegisterMicrosoftClient(
-            //    clientId: "",
-            //    clientSecret: "");
-
-            //OAuthWebSecurity.RegisterTwitterClient(
-            //    consumerKey: "",
-            //    consumerSecret: "");
-
-            //OAuthWebSecurity.RegisterFacebookClient(
-            //    appId: "",
-            //    appSecret: "");
-
-            //OAuthWebSecurity.RegisterGoogleClient();
+            DataSet dataSet = DB.ExecuteDataset("SELECT clientID,authorityurl,tenantoptional,redirecturi,Provider,AD_Ref_List.value,ad_ref_list.name FROM sso_configuration \r\n                        INNER JOIN AD_Ref_List  ON AD_Ref_List.Value=sso_configuration.Provider\r\n                        WHERE sso_configuration.IsActive='Y' AND AD_Reference_ID IN (\r\n                        SELECT ad_reference_ID FROM ad_reference WHERE NAme='VIS_ServiceProvider'\r\n                        )");
+            AppBuilderSecurityExtensions.SetDefaultSignInAsAuthenticationType(app, "Cookies");
+            CookieAuthenticationExtensions.UseCookieAuthentication(app, new CookieAuthenticationOptions()
+            {
+                CookieManager = (ICookieManager)new SystemWebCookieManager(),
+                LoginPath = new PathString("/Account/JsonLogin"),
+                LogoutPath = new PathString("/")
+            });
+            IAppBuilder iappBuilder1 = app;
+            CookieAuthenticationOptions authenticationOptions1 = new CookieAuthenticationOptions();
+            ((AuthenticationOptions)authenticationOptions1).AuthenticationType = "ApplicationCookie";
+            authenticationOptions1.LoginPath = new PathString("/Account/JsonLogin");
+            authenticationOptions1.LogoutPath = new PathString("/");
+            CookieAuthenticationExtensions.UseCookieAuthentication(iappBuilder1, authenticationOptions1);
+            if (dataSet == null || dataSet.Tables[0].Rows.Count <= 0)
+                return;
+            for (int index = 0; index < dataSet.Tables[0].Rows.Count; ++index)
+            {
+                string str = string.Format((IFormatProvider)CultureInfo.InvariantCulture, Util.GetValueOfString(dataSet.Tables[0].Rows[index]["authorityurl"]), (object)Util.GetValueOfString(dataSet.Tables[0].Rows[index]["tenantoptional"]));
+                IAppBuilder iappBuilder2 = app;
+                OpenIdConnectAuthenticationOptions authenticationOptions2 = new OpenIdConnectAuthenticationOptions();
+                ((AuthenticationOptions)authenticationOptions2).AuthenticationType = Util.GetValueOfString(dataSet.Tables[0].Rows[index]["value"]).ToLower();
+                authenticationOptions2.ClientId = Util.GetValueOfString(dataSet.Tables[0].Rows[index]["clientID"]);
+                authenticationOptions2.Authority = str;
+                authenticationOptions2.RedirectUri = Util.GetValueOfString(dataSet.Tables[0].Rows[index]["redirecturi"]);
+                authenticationOptions2.PostLogoutRedirectUri = Util.GetValueOfString(dataSet.Tables[0].Rows[index]["redirecturi"]);
+                authenticationOptions2.Scope = "openid profile";
+                authenticationOptions2.ResponseType = "code id_token";
+                authenticationOptions2.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false
+                };
+                authenticationOptions2.Notifications = new OpenIdConnectAuthenticationNotifications();
+                OpenIdConnectAuthenticationExtensions.UseOpenIdConnectAuthentication(iappBuilder2, authenticationOptions2);
+            }
         }
     }
 }
---
+++ ViennaBase/Global.asax.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using VAdvantage.Classes;
+using VAdvantage.Utility;
+
+namespace ViennaBase
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier";
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+          SessionEventHandler.SessionEnd(Session["ctx"] as Ctx, Session.SessionID);
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
